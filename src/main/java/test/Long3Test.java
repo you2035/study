5d04911a10ac3efd4caa04c0f8b990d3687f5574
liuxin <1649096790@qq.com>
2020-02-23 20:55:33 +0800

    long3工具类

---
+++ src/main/java/test/Long3Test.java
@@ -0,0 +1,12 @@
+package test;
+
+
+import org.apache.commons.lang.StringUtils;
+
+public class Long3Test {
+  public static void main(String[] args) {
+    String s= "";
+    boolean isBlank = StringUtils.isBlank(s);
+    System.out.println(isBlank);
+  }
+}
