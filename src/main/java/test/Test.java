03c3888758b37a65ab8d0e1b985c66a29c4e2090
liuxin <1649096790@qq.com>
2020-06-16 15:02:07 +0800

    随机汉字生成

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,9 +1,37 @@
 package test;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.channels.SelectionKey;
+import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
+//        String s = "123我是谁";
+//        char[] chars = s.toCharArray();
+//        for (char aChar : chars) {
+//            System.out.println(Character.isDigit(aChar));
+//            System.out.println(aChar);
+//        }
+        System.out.println(getRandomChar());
         System.out.println(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+    }
+
+    private static char getRandomChar() {
+        String str = "";
+        int hightPos; //
+        int lowPos;
+        Random random = new Random();
+        hightPos = (176 + Math.abs(random.nextInt(39)));
+        lowPos = (161 + Math.abs(random.nextInt(93)));
+        byte[] b = new byte[2];
+        b[0] = (Integer.valueOf(hightPos)).byteValue();
+        b[1] = (Integer.valueOf(lowPos)).byteValue();
+        try {
+            str = new String(b, "GBK");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            System.out.println("错误");
+        }
+        return str.charAt(0);
     }
 }
