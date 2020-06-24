7fd493b3bc50f6da9b0598e713b68cc7496afe58
liuxin <1649096790@qq.com>
2020-06-24 15:32:40 +0800

    测试

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,19 +1,35 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.channels.SelectionKey;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        String s = "123我是谁";
 //        char[] chars = s.toCharArray();
 //        for (char aChar : chars) {
 //            System.out.println(Character.isDigit(aChar));
 //            System.out.println(aChar);
 //        }
-        System.out.println(getRandomChar());
-        System.out.println(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+//        System.out.println(getRandomChar());
+//        System.out.println(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+//        System.out.println(42 == 42.0);
+//        Test test = new Test();
+//        Test test1 = new Test();
+//        System.out.println(test == test1);
+        System.out.println("请输入数字：");
+//        Scanner scanner = new Scanner(System.in);
+//        String s = scanner.nextLine();
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String s = input.readLine();
+        System.out.println("你输入的数字是 ："+s);
+
     }
 
     private static char getRandomChar() {
